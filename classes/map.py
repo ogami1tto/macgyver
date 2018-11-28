6dc93d51bd56a3fafad16e09c68c7eb804346130
Ogami <39329325+ogami1tto@users.noreply.github.com>
2018-11-28 12:36:20 +0100

    Update map.py

--- classes/map.py
+++ classes/map.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python3
 # coding: utf-8
 
+# Faut-il regrouper toutes les classes en un seul fichier ou bien créer des fichiers distincts pour chaque classe ???
+
 class Map:
     """classe qui construit la carte du labyrinthe"""
     
@@ -8,21 +10,19 @@ class Map:
         """"constructeur de classe"""
         self.level = level
 
-
     def create_map(self):
         """methode permettant de transformer le fichier texte en un dictionnaire"""
-        with open(level, "r") as map_file:
+        with open(level, "r") as map_file:           # lecture du fichier texte contenant le 'plan' du labyrinthe
             x = 0
             y = 0
             map_dict = {}
-            for line in map_file:
+            for line in map_file:                    # les données du fichier texte sont stockées dans un dictionnaire
                 for char in line:
                     if char != '\n':
                         map_dict[str(x), str(y)] = char
                     x += 1
                 y += 1
                 x = 0
-
         
     def display_map():
         """méthode qui génère l'interface graphique à partir du dictionnaire"""
@@ -31,7 +31,7 @@ class Map:
  class Item:
     """classe qui va gérer les items du jeu"""
     
-    def __init__(self, level): # ajouter "level" aux paramètres ??
+    def __init__(self, level):                       # ajouter "level" aux paramètres ??
         """constructeur de classe"""
         
     def random_item(self):
@@ -39,13 +39,14 @@ class Map:
         
     def display_item(self):
         """méthode qui affiche graphiquement les items sur la carte"""
-        # Le Gardien est-il considéré comme un item ???
+                                                     # Le Gardien est-il considéré comme un item ???
+                                                     # Faire disparaître les items une fois collectés par le personnage !!!
         
         
  class Character:
     """classe qui gère le personnage contrôlé par le joueur"""
     
-    def __init__(self, level): # ajouter "level" aux paramètres ??
+    def __init__(self, level):                       # ajouter "level" aux paramètres ??
         """constructeur de classe"""
         
     def spawn_char(self):

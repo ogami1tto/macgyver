05239cd084230ab175d19ac13db1bbd977877789
Ogami <ogami1tto@protonmail.com>
2018-11-29 08:43:53 +0100

    Creation of files 'item.py' and 'characer.py' inside 'class' folder.

---
+++ classes/character.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
+
+class Character:
+    """classe qui gère le personnage contrôlé par le joueur"""
+    
+    def __init__(self, level):                       # ajouter "level" aux paramètres ??
+        """constructeur de classe"""
+        
+    def spawn_char(self):
+        """méthode qui fait apparaître le personnage à un endroit déterminé"""
+        
+    def move_char(self):
+        """méthode qui permet d'autoriser les déplacements du personnage dans le labyrinthe"""
+        
+    def display_char(self):
+        """méthode qui affiche graphiquement le personnage sur la carte"""
+        
---
+++ classes/item.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
+
+class Item:
+    """classe qui va gérer les items du jeu"""
+    
+    def __init__(self, level):                       # ajouter "level" aux paramètres ??
+        """constructeur de classe"""
+        
+    def random_item(self):
+        """méthode qui place aléatoire les différents items sur la carte"""
+        
+    def display_item(self):
+        """méthode qui affiche graphiquement les items sur la carte"""
+                                                     # Le Gardien est-il considéré comme un item ???
+                                                     # Faire disparaître les items une fois collectés par le personnage !!!
+        
+        

0a47b8883ba8a2fef4d6bbcc6bc402b70ea6fac5
Ogami <ogami1tto@protonmail.com>
2018-12-01 16:28:56 +0100

    done

--- classes/character.py
+++ classes/character.py
@@ -3,17 +3,32 @@
 
 
 class Character:
-    """classe qui gère le personnage contrôlé par le joueur"""
+    """classe qui gère les personnages"""
+
+# si Mc Gyger sont deux objets de la même classe, doit-on créer des classes 
+# fillesnpour chacun d'eux ?
     
-    def __init__(self, level):                       # ajouter "level" aux paramètres ??
+    def __init__(self, s):
         """constructeur de classe"""
+        self.starting_point = s # point de départ sur la map
+        self.image = picture # image associé au personnage
+        self.status = false or true # statut du personnage  
+
+        #pour Mc GYver:
+        # self.items_collected = []
         
-    def spawn_char(self):
+    # def spawn_char(self):
         """méthode qui fait apparaître le personnage à un endroit déterminé"""
+        # lettre speciale dans les valeurs du dictionnaire LABY_FILE
+
+    def pick_item(self):
+    	"""Methode qui permet de collecter un objet en passant dessus"""
         
     def move_char(self):
         """méthode qui permet d'autoriser les déplacements du personnage dans le labyrinthe"""
         
-    def display_char(self):
-        """méthode qui affiche graphiquement le personnage sur la carte"""
+    def die_char(self):
+        """méthode qui agit sur le statut du personnage"""
         
+if __name__ == '__main__':
+	mcgyver = Character()
--- classes/map.py
+++ classes/map.py
@@ -8,34 +8,72 @@ class Map:
     
     def __init__(self):
         """"constructeur de classe"""
-        self.level_file = config.LABY_FILE
-        self.map_dict = {}
+        self.level_file = config.LABY_FILE  # fichier texte où se trouve le laby.
+        self.map_dict = {}    # dictionnaire où sont stockées les cases du laby.
+        self.free_frame = []  # liste contenant les cases vides.
 
     def create_map(self):
         """methode permettant de transformer le fichier texte en un dictionnaire"""
-        with open(self.level_file, "r") as level:           # lecture du fichier texte contenant le 'plan' du labyrinthe
+        with open(self.level_file, "r") as level:    # lecture du fichier texte contenant le 'plan' du labyrinthe
             x = 0
             y = 0
-            for line in level:                    # les données du fichier texte sont stockées dans un dictionnaire
+            for line in level:                       
                 for char in line:
                     if char != '\n':
-                        self.map_dict[str(x), str(y)] = char
+                        self.map_dict[str(x), str(y)] = char # les données du fichier texte sont stockées dans un dictionnaire
                     x += 1
                 y += 1
                 x = 0
 
 
+    def empty_frame(self):
+        """methode qui propose de stocker dans une liste les coordonnées des cases vides du laby"""
+        for cle, valeur in self.map_dict.items():
+            if valeur == "o":
+                self.free_frame.append(cle)
+
+
+
 if __name__ == '__main__':
     m = Map()
-    print(m.map_dict)
+    # print(m.map_dict)
     m.create_map()
-    print(m.map_dict)
+    # print(m.map_dict)
+    m.empty_frame()
+    print(m.free_frame)
+
+    # for cle, valeur in m.map_dict.items():
+    #     if valeur == "o":
+    #         print(cle)
 
 
+    # for valeur in m.map_dict.values():
+    #     if valeur == 'o':
+    #         print(m.map_dict.keys())
+    
+    
 
-    # def display_map():
-    #     """méthode qui génère l'interface graphique à partir du dictionnaire"""
-        
+    # def empty_frame(self):
+    #     """méthode qui récupère les cases vides du labyrinthe"""
+
+
+# def letter_reader(self):
+#   dico = {}
+#   fichier = open("/Users/florent/Dropbox/PYTHON EXOS/macgyver/map/map.txt", "r")
+#   x = 0
+#   y = 0
+#   for ligne in fichier:  # position de y
+#       for carac in ligne:  # position de x
+#           if carac not in "\n":  # pour ne pas inclure les sauts de ligne dans le décompte
+#               if carac == self:
+#                   dico[str(x), str(y)] = carac
+#           x += 1
+#       y += 1
+#       x = 0
+#
+#   print(dico)
+#
+# letter_reader("s")
 
  
 

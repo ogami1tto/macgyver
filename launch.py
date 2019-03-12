d5fe4239f1ce4fd3d0933fc66d5b0bf923b5c0a4
Ogami <ogami1tto@protonmail.com>
2019-03-12 15:26:54 +0100

    12/03

--- launch.py
+++ launch.py
@@ -11,5 +11,7 @@ Fichiers : config.py, game.py, guardian.py, item.py, map.py, player.py, position
 
 from classes import game
 
-if __name__ == '__main__':
-    new_game = game.Game()
+
+
+new_game = Game()
+new_game.run_pygame()
